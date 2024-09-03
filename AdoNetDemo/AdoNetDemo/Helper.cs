69249a3833d7c54f185302b88d3bfee42484c307
RanjanBhatnagar <67755323+RanjanBhatnagar@users.noreply.github.com>
2024-09-03 18:47:40 +0530

    ADO.NET Daemo Reference

---
+++ AdoNetDemo/AdoNetDemo/Helper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetDemo
+{
+    public class Helper
+    {
+        public static string ConnectionString
+        {
+            get
+            {
+                return @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=USTDemoDB;Integrated Security=True";
+            }
+        }
+    }
+}
