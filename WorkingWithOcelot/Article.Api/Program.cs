2199f2c6b4251bbb9834f2359ef5235a97ba3ff3
RanjanBhatnagar <67755323+RanjanBhatnagar@users.noreply.github.com>
2024-11-04 12:53:06 +0530

    Sample to create ocelot project

---
+++ WorkingWithOcelot/Article.Api/Program.cs
@@ -0,0 +1,41 @@
+
+using Article.Api.Repository;
+
+namespace Article.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+            builder.Services.AddScoped<IArticleRepository, ArticleRepository>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ WorkingWithOcelot/Article.Api/Repository/IArticleRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Article.Api.Repository
+{
+    public interface IArticleRepository
+    {
+        int Delete(int id);
+        Models.Article? Get(int id);
+        List<Models.Article> GetAll();
+    }
+}
